0be34136b97eafcbb72cf9eea25462cff96d437d
unknown <lv1018@GDD-61.main.ad.rit.edu>
2015-02-14 17:19:21 -0500

    Lakshminarayanan

---
+++ BitvataiCaliber/Assets/Scripts/Grid.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour {
+	public int x,y;
+	public float check;
+	public int cubeNum;
+	public GameObject japPlayer;
+	public GameObject rusPlayer;
+	public GameObject[,] position = new GameObject [6,5];
+	public int [] a = new int[5];
+	
+	// Use this for initialization
+	void Start () {
+		Instantiate (japPlayer);
+		Instantiate (rusPlayer);
+		japPlayer.transform.position = new Vector3 (-4f, 7f, -1f);
+		rusPlayer.transform.position = new Vector3 (-4f, -3f, -1f);
+		CreateWorld ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void CreateWorld()
+	{
+		
+		for (int i = 0; i <6; i++) {
+			
+			for (int j = 0; j < 5; j++) {
+				
+				GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				cube.name = "Box"+ cubeNum;
+				cube.transform.position = new Vector3 (x+=2,y,0);
+				cube.transform.parent = this.transform;
+				cube.renderer.material.color = new Color(Random.Range (0f,10f) / 10, Random.Range (0f,10f) / 10, Random.Range (0f,10f) / 10);
+				position[i,j]= cube;
+				cubeNum++;
+			}
+			y-=2;
+			x=-6;
+			
+		}
+		
+		Debug.Log (position.Length);
+	}
+}
---
+++ BitvataiCaliber/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : Grid {
+
+	public GameObject[,] neighborNodes = new GameObject[2,2]
+	void Start () {
+	
+
+	}
+
+	void MaptheBoxes()
+	{
+
+		foreach (GameObject boxes in position) 
+		{
+
+		}
+
+	}
+
+
+}
