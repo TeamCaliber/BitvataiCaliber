e62397566b1291477cd69af3be0151e6426157b9
unknown <lv1018@GDD-61.main.ad.rit.edu>
2015-02-14 21:29:11 -0500

    Lakshmifinalminutes

--- BitvataiCaliber/Assets/Resources/Boxes.cs
+++ BitvataiCaliber/Assets/Resources/Boxes.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 
 public struct boxes{
@@ -12,16 +12,48 @@ public struct boxes{
 public class Boxes : MonoBehaviour {
 
 	public boxes box;
-	
+	public bool isPlayerOnthis = false;
+	public List<GameObject> boxList = new List<GameObject>();
+	public bool Clickable;
 	// Use this for initialization
-	void Start () {
+	void Start () {	
 	
-						
+		if(boxList.Count < 4)
+		{
+			boxList.Add(box.left);
+			boxList.Add(box.top);
+			boxList.Add(box.down);
+			boxList.Add(box.right);
+			
+		}
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
-//	Debug.Log(box.left.name);
+		if(isPlayerOnthis == true)
+		{
+			foreach (GameObject boxes in boxList)
+			{	if(boxes!=null)
+				boxes.GetComponent<Boxes>().Clickable = true;
+			}
+		
+		}
+		
+		if (Input.GetButtonDown("Fire1")) {
+		Debug.Log("asD");
+			RaycastHit hit;
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+			if (Physics.Raycast(Input.mousePosition,fwd,out hit))
+			{
+					Debug.Log(hit.transform.name);
+			}
+			
+				
+				
+		}
 	}
 }
+	
