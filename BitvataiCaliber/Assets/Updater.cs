1c53a199e586fa8b46d027125bc24a4b80371bc0
Argzero <forrest.z.shooster@gmail.com>
2015-02-17 04:18:44 -0500

    Veryclose to finished but not quite

---
+++ BitvataiCaliber/Assets/Updater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Updater : MonoBehaviour {
+
+	public PlayerManager Players;
+	// Use this for initialization
+	void Start () {
+		Players = (GameObject.FindWithTag ("PlayerManager")).GetComponent<PlayerManager> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		UpdateValues ();
+	}
+	
+	void UpdateValues(){
+		Debug.Log ("VALUES");	
+
+	}
+}
